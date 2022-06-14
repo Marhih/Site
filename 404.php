02eb48a6c0ba551613813028f0b4663619cb83b7
Marielen <95556847+Marhih@users.noreply.github.com>
2022-06-14 15:27:38 -0300

    Página de erro

---
+++ 404.php
@@ -0,0 +1,8 @@
+<section class="erro-404">
+	<div class="center">
+
+		<h2><i style="padding: 0 20px;" class="fa-solid fa-xmark"></i>Desculpe!</h2>
+		<h2>Estou trabalhando para resolver esse erro!</h2>
+
+	</div><!--center-->
+</section>
