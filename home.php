9a31db9b2d9e950f985799bc1c6d9bead3025b58
Marielen <95556847+Marhih@users.noreply.github.com>
2022-06-14 15:28:40 -0300

    Página de Home

---
+++ home.php
@@ -0,0 +1,86 @@
+<section class="banner-principal">
+		<div class="overlay"></div><!--overlay-->
+		<div class="center">
+		<form>
+			<h2>Qual seu email?</h2>
+			<input type="email" name="email" required />
+			<input type="submit" name="acao" value="Cadastrar!">
+		</form>
+		</div><!--center-->
+	</section><!--banner-principal-->
+
+	<section class="descricao-autor">
+		<div class="center">
+		<div class="w50 left">
+			<h2>Mari Rez</h2>
+			<p>Oi, eu sou Mari!</p>
+			<p>Comecei no mundo da programação recentemente, mas apesar de está apenas começando, eu já adquiri muito conhecimento nessa área. Estou no 2º semestre de Análise e Desenvolvimento de Sistemas, mas iniciei nesse mundo com cursos voltados para a programação. Meu primeiro contato com a programação foi com o Backend, mas precisamente com a linguagem Python, onde me apaixonei por TI. Atualmente tenho conhecimento para me considerar uma desenvolvedora Full-Stack. </p>
+		</div><!--w50-->
+		<div class="w50 left eu">
+			<img src="<?php echo INCLUDE_PATH; ?>images/marirrr.jpg" width="300" height="300" />
+		</div><!--eu-->
+		<div class="clear"></div>
+		</div><!--center-->
+	</section><!--descricao-autor-->
+
+	<section class="especialidade">
+		<h2 class="title">Especialidades</h2>
+		<div class="center">
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa-solid fa-code"></i></h3>
+				<h4>FRONTEND</h4>
+				<ul>
+					<li>HTML5</li>
+					<li>CSS3</li>
+					<li>JavaScript</li>
+				</ul>
+			</div><!--box-especialidade-->
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa-solid fa-server"></i></h3>
+				<h4>BACKEND</h4>
+				<ul>
+					<li>Python</li>
+					<li>PHP</li>
+					<li>NodeJs</li>
+				</ul>
+			</div><!--box-especialidade-->
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa-solid fa-database"></i></h3>
+				<h4>DATABASE/CLOUD</h4>
+				<ul>
+					<li>MySQL</li>
+					<li>MongoDB</li>
+					<li>AWS</li>
+				</ul>
+			</div><!--box-especialidade-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--especialidade-->
+
+	<section class="sobre">
+		<h2 class="title">Sobre Mim</h2>
+		<div class="center">
+			<div class="w50 right badge-base LI-profile-badge" data-locale="pt_BR" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="marielen-rezende-733271163" data-version="v1">
+			</div><!--badge-base LI-profile-badge-->
+			<div class="w50 right github">
+				<a href="https://github.com/Marhih"><img src="images/github.png"></a>	
+			</div>
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--sobre-->
+
+	<section class="habilidades">
+		<h2 class="title">Habilidades</h2>
+		<div class="center">
+		<div class="w33 left servicos-container">
+			<h3>Domínio em Git e GitHub</h3>
+		</div><!--servicos-container-->
+		<div class="w33 left servicos-container">
+			<h3>Metologias Ágeis: Scrum, Kaban, XP</h3>
+		</div><!--servicos-container-->
+		<div class="w33 left servicos-container">
+			<h3>Implementação de API's</h3>
+		</div><!--servicos-container-->
+			<div class="clear"></div>
+		</div><!--habilidades-->
+	</section>
