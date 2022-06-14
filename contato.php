19b8b1b5c5c5c18686e61b689de361b85824115f
Marielen <95556847+Marhih@users.noreply.github.com>
2022-06-14 15:28:12 -0300

    Página de Contato

---
+++ contato.php
@@ -0,0 +1,11 @@
+<div class="contato-container">
+	<div class="center">
+		<form>
+			<input type="text" name="nome" placeholder="Nome..."><br>
+			<input type="text" name="email" placeholder="Email.."><br>
+			<input type="text" name="telefone" placeholder="Telefone.."><br>
+			<textarea name="mensagem"></textarea><br>
+			<input type="submit" name="acao" value="Enviar">
+		</form>
+	</div><!--center-->
+</div><!--contato-container-->
